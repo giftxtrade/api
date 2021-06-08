aa61fcdb4a8a1251cafddb7eed77fa6a1f1145e1
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-08 15:48:43 -0500

    Add frontend routes

---
+++ src/util/external-routes.ts
@@ -0,0 +1,7 @@
+export const frontend_base = 'http://localhost:3000/';
+
+export const frontend = {
+  home: frontend_base,
+  google: `${frontend_base}auth/google`,
+  twitter: `${frontend_base}auth/twitter`,
+}

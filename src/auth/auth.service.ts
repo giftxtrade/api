062e854ad22e643487de36e9641eaab6daed57a0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-08 20:19:47 -0500

    Implement auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userServices: UsersService,
+    private readonly jwtService: JwtService
+  ) { }
+
+  async validateUser(email: string, pass: string): Promise<User> {
+    return this.userServices.findOne(email);
+  }
+
+  async login(createUser: CreateUserDto, accessToken: string): Promise<{ user: User, accessToken: string }> {
+    const user = await this.userServices.findOneOrCreate(createUser);
+    const payload = { user, accessToken };
+    return {
+      user: user,
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}

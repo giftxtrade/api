ba4d9c3cf0321ea444437895db1f5a2ec06246a6
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-08 19:44:35 -0500

    Implement users services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) { }
 
+  async insert(user: CreateUserDto): Promise<User> {
+    const userEntity = new User();
+    userEntity.email = user.email;
+    userEntity.name = user.name;
+    userEntity.imageUrl = user.imageUrl
+
+    return await userEntity.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async findOne(email: string): Promise<User> {
+    return await this.usersRepository
+      .findOne({
+        where: { email: email }
+      });
+  }
+
+  async findOneOrCreate(user: CreateUserDto): Promise<User> {
+    const existingUser = await this.findOne(user.email);
+    if (existingUser)
+      return existingUser;
+    return await this.insert(user);
+  }
+
+  async remove(email: string): Promise<DeleteResult> {
+    return await this.usersRepository.delete(email);
+  }
 }

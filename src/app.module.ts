78d62fc3a508eef47213de72a092e7a892ee2ba3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-11 16:19:20 -0500

    Add products module + controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GoogleModule } from './google/google.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     UsersModule,
     GoogleModule,
-    AuthModule
+    AuthModule,
+    ProductsModule
   ],
   controllers: [AppController],
   providers: [AppService],

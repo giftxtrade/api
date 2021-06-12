ed59c073363b7d312c98d953c2be72ac2db35ce6
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-11 20:24:40 -0500

    Products: Import CategoriesService

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,12 +3,14 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { CategoriesService } from 'src/categories/categories.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Product
-    ])
+    ]),
+    CategoriesService,
   ],
   controllers: [ProductsController],
   providers: [ProductsService],

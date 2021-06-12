ded689e7b1dddb57b6f5e3a4313857a4a78d3989
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-11 20:24:51 -0500

    Product: Add services

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { CategoriesService } from '../categories/categories.service';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    private readonly categoryServices: CategoriesService,
+  ) { }
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const product = new Product();
+    product.title = createProductDto.title;
+    product.description = createProductDto.description;
+    product.productKey = createProductDto.productKey;
+    product.imageUrl = createProductDto.imageUrl;
+    product.rating = createProductDto.rating;
+    product.price = createProductDto.price;
+    product.currency = createProductDto.currency;
+    product.category = await this.categoryServices.findLikeOrCreate(createProductDto.category);
+    product.website = createProductDto.website;
+
+    return await product.save();
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(): Promise<Product[]> {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number): Promise<Product> {
+    return await this.productRepository.findOne(id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  /* update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} product`;
-  }
+  } */
 }

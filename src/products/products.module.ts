39d724d7e3aa77a631671ca774ee92a35199c6e3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-11 21:22:04 -0500

    Fix Products: Replace CategoriesService with CategoriesModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,14 +3,14 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { CategoriesService } from 'src/categories/categories.service';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Product
     ]),
-    CategoriesService,
+    CategoriesModule,
   ],
   controllers: [ProductsController],
   providers: [ProductsService],

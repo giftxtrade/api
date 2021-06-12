21a0dcc0d32373f285e0ed7105a11615ea6354c9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-11 20:05:01 -0500

    Category: Add find or create methods to service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -19,6 +19,11 @@ export class CategoriesService {
   }
 
   async findOne(name: string): Promise<Category> {
+    return await this.categoryRepository
+      .findOne({ where: { name: name } });
+  }
+
+  async findOneLike(name: string): Promise<Category> {
     name = name.toLowerCase().trim();
     return this.categoryRepository
       .createQueryBuilder('categories')
@@ -27,5 +32,19 @@ export class CategoriesService {
         description like :name
       `, { name: `%${name}%` })
       .getOne();
+  }
+
+  async findOrCreate(name: string, description?: string, categoryUrl?: string): Promise<Category> {
+    const category = await this.findOne(name);
+    if (!category)
+      return await this.insert(name);
+    return category;
+  }
+
+  async findLikeOrCreate(name: string, description?: string, categoryUrl?: string): Promise<Category> {
+    const category = await this.findLikeOrCreate(name);
+    if (!category)
+      return await this.insert(name);
+    return category;
   }
 }

a8f3738c9c246bef23e89d7c2d5f27b1bb0bbc06
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-11 23:09:43 -0500

    Product: Throw 404 exception if product is not found

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, HttpException, HttpStatus } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -21,7 +21,13 @@ export class ProductsController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return await this.productsService.findOne(+id);
+    const product = await this.productsService.findOne(+id);
+    if (!product) {
+      throw new HttpException({
+        messsage: 'Product not found'
+      }, HttpStatus.NOT_FOUND)
+    }
+    return product;
   }
 
   /* @Patch(':id')

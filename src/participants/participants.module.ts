ee564e578840785865754dd88915be98fdad42f5
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-17 14:12:14 -0500

    Add entites to modules

--- src/participants/participants.module.ts
+++ src/participants/participants.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ParticipantsService } from './participants.service';
 import { ParticipantsController } from './participants.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Participant } from './entities/participant.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Participant
+    ])
+  ],
   controllers: [ParticipantsController],
   providers: [ParticipantsService]
 })
--- src/wishes/wishes.module.ts
+++ src/wishes/wishes.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { WishesController } from './wishes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wish } from './entities/wish.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Wish
+    ])
+  ],
   controllers: [WishesController],
   providers: [WishesService]
 })

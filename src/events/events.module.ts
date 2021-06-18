db82ebcc1fa7756a5f00755238a003a63ac1389a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-18 15:36:00 -0500

    Events: Add imports and exports

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,14 +3,19 @@ import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
+import { ParticipantsModule } from 'src/participants/participants.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Event
-    ])
+    ]),
+    ParticipantsModule,
+    UsersModule
   ],
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService],
+  exports: [EventsService]
 })
 export class EventsModule {}

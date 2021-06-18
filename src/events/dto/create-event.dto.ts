a4a9a570fa075bb0972293b8fd877cbacbc4b263
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-17 22:47:52 -0500

    Events: Add CreateEventDto

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,9 @@
-export class CreateEventDto {}
+export class CreateEventDto {
+  name: string;
+  description: string;
+  budget: number;
+  invitationMessage: string;
+  drawAt: Date;
+  closeAt: Date;
+  participants: { name: string, email: string }[]
+}

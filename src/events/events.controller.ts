667a7b612763c56101436e40d0b3b55fdb8fa0f3
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-18 19:34:29 -0500

    Events: Add [POST] /create route

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UsersService } from 'src/users/users.service';
+import { Event } from './entities/event.entity';
 
 @Controller('events')
 export class EventsController {
-  constructor(private readonly eventsService: EventsService) {}
+  constructor(
+    private readonly eventsService: EventsService,
+    private readonly usersService: UsersService
+  ) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createEventDto: CreateEventDto) {
-    return this.eventsService.create(createEventDto);
+  async create(@Request() req, @Body() createEventDto: CreateEventDto): Promise<Event> {
+    const user = await this.usersService.findOne(req.user.user.email);
+    return await this.eventsService
+      .create(createEventDto, user);
   }
 
   @Get()

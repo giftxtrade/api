5918d87af3ca9c810637ef188043b6ed03f375b1
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-20 15:42:57 -0500

    Events: Add [GET] /invites/accept/:eventId route

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, HttpStatus, HttpException } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UsersService } from 'src/users/users.service';
 import { Event } from './entities/event.entity';
+import { ParticipantsService } from '../participants/participants.service';
 
 @Controller('events')
 export class EventsController {
   constructor(
     private readonly eventsService: EventsService,
-    private readonly usersService: UsersService
+    private readonly usersService: UsersService,
+    private readonly participantsService: ParticipantsService,
   ) { }
 
   @UseGuards(JwtAuthGuard)
@@ -23,21 +25,38 @@ export class EventsController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  async findAll(@Request() req) {
+  async findAll(@Request() req): Promise<Event[]> {
     const user = await this.usersService.findByEmail(req.user.user.email);
     return await this.eventsService.findAllForUser(user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('/invites')
-  async findAllInvites(@Request() req) {
+  async findAllInvites(@Request() req): Promise<Event[]> {
     const user = await this.usersService.findByEmail(req.user.user.email);
     return await this.eventsService.findAllInvitesForUser(user);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('/invites/accept/:eventId')
+  async acceptInvite(@Request() req, @Param('eventId') eventId: number): Promise<Event> {
+    const user = await this.usersService.findByEmail(req.user.user.email);
+    const event = await this.eventsService.findOne(eventId);
+    if (!event) {
+      throw new HttpException({
+        message: 'Event not found'
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    const participant = await this.participantsService.acceptEvent(event, user)
+    return await this.eventsService.findOneForUser(eventId, user);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.eventsService.findOne(+id);
+  async findOne(@Request() req, @Param('eventId') eventId: number): Promise<Event> {
+    const user = await this.usersService.findByEmail(req.user.user.email);
+    return await this.eventsService.findOneForUser(eventId, user);
   }
 
   @Patch(':id')

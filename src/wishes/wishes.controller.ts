451a88b93b19092d8a7b567bed8fb67c690d97ff
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-24 16:15:44 -0500

    Wishes: Add controller

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,34 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, HttpException, HttpStatus } from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { UpdateWishDto } from './dto/update-wish.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UsersService } from 'src/users/users.service';
+import { EventsService } from 'src/events/events.service';
 
 @Controller('wishes')
 export class WishesController {
-  constructor(private readonly wishesService: WishesService) {}
+  constructor(
+    private readonly wishesService: WishesService,
+    private readonly usersService: UsersService,
+    private readonly eventsService: EventsService,
+  ) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createWishDto: CreateWishDto) {
-    return this.wishesService.create(createWishDto);
+  async create(@Request() req, @Body() createWishDto: CreateWishDto) {
+    const user = await this.usersService.findByEmail(req.user.user.email);
+    return await this.wishesService.create(user, createWishDto);
   }
 
-  @Get()
-  findAll() {
-    return this.wishesService.findAll();
-  }
-
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.wishesService.findOne(+id);
+  async findAll(@Request() req, @Param('id') eventId) {
+    const user = await this.usersService.findByEmail(req.user.user.email);
+    const event = await this.eventsService.findOneForUser(eventId, user);
+    if (!event) {
+      throw new HttpException({
+        message: 'Event not found'
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    return await this.wishesService.findAllByUserEvent(user, event);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWishDto: UpdateWishDto) {
-    return this.wishesService.update(+id, updateWishDto);
-  }
-
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wishesService.remove(+id);
+  async remove(@Request() req, @Param('id') id: number, @Body() createWishDto: CreateWishDto) {
+    const user = await this.usersService.findByEmail(req.user.user.email);
+    return await this.wishesService.remove(user, id, createWishDto);
   }
 }

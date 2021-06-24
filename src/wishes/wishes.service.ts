995e33584969246ee1db934030e5cdc9bd7cc595
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-24 15:39:36 -0500

    Wishes: Add services

--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,26 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { UpdateWishDto } from './dto/update-wish.dto';
+import { EventsService } from 'src/events/events.service';
+import { ProductsService } from 'src/products/products.service';
+import { ParticipantsService } from 'src/participants/participants.service';
+import { User } from 'src/users/entities/user.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
+import { Product } from 'src/products/entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Event } from 'src/events/entities/event.entity';
 
 @Injectable()
 export class WishesService {
-  create(createWishDto: CreateWishDto) {
-    return 'This action adds a new wish';
+  constructor(
+    @InjectRepository(Wish)
+    private readonly wishRepository: Repository<Wish>,
+    private readonly eventsService: EventsService,
+    private readonly productsService: ProductsService,
+    private readonly participantsService: ParticipantsService,
+  ) { }
+
+  async create(user: User, createWishDto: CreateWishDto) {
+    const { event, participant, product } = await this
+      .getWishItems(user, createWishDto.eventId, createWishDto.productId, createWishDto.participantId);
+
+    const wish = new Wish()
+    wish.user = user;
+    wish.event = event;
+    wish.participant = participant;
+    wish.product = product;
+    return await wish.save();
   }
 
-  findAll() {
-    return `This action returns all wishes`;
+  findAllByUserEvent(user: User, event: Event): Promise<Wish[]> {
+    return this.wishRepository
+      .createQueryBuilder('w')
+      .leftJoinAndSelect('w.product', 'products')
+      .where('w.userId = :userId AND w.eventId = :eventId', {
+        userId: user.id,
+        eventId: event.id
+      })
+      .getMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} wish`;
+  async remove(user: User, wishId: number, createWishDto: CreateWishDto) {
+    const { product, event, participant } = await this
+      .getWishItems(user, createWishDto.eventId, createWishDto.productId, createWishDto.participantId);
+
+    if (!product || !event || !participant) {
+      throw new HttpException({
+        message: 'Something went wrong'
+      }, HttpStatus.BAD_REQUEST);
+    }
+    return this.wishRepository.delete(wishId);
   }
 
-  update(id: number, updateWishDto: UpdateWishDto) {
-    return `This action updates a #${id} wish`;
-  }
+  private async getWishItems(user: User, eventId: number, productId: number, participantId: number) {
+    const event = await this.eventsService.findOneForUser(eventId, user);
+    const product = await this.productsService.findOne(productId);
 
-  remove(id: number) {
-    return `This action removes a #${id} wish`;
+    if (!event || !product) {
+      throw new HttpException({
+        message: 'Could not find event or product'
+      }, HttpStatus.NOT_FOUND);
+    }
+
+    const participant = await this.participantsService.findByEventAndUser(event, user);
+    if (!participant) {
+      if (participant.organizer && !participant.participates) {
+        throw new HttpException({
+          message: 'You are not a participant for this group. Swith your status to participant if you want a wishlist'
+        }, HttpStatus.BAD_REQUEST);
+      }
+
+      throw new HttpException({
+        message: 'You are not part of this event'
+      }, HttpStatus.BAD_REQUEST);
+    }
+
+    if (participant.id !== participantId) {
+      throw new HttpException({
+        message: 'Invalid participant id'
+      }, HttpStatus.BAD_REQUEST);
+    }
+
+    return { event, participant, product };
   }
 }

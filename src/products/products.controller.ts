8ce1ab9fa1f7f2c6f5c232781873feb7103f52a7
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-24 21:27:45 -0500

    Products: Remove commented methods

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -49,14 +49,4 @@ export class ProductsController {
     }
     return product;
   }
-
-  /* @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
-  } */
 }

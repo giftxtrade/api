cd8605e6da4643f289306f025355119ca3cbe319
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-29 15:50:10 -0500

    Draws: Fix module imports

--- src/draws/draws.module.ts
+++ src/draws/draws.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { DrawsService } from './draws.service';
 import { DrawsController } from './draws.controller';
 import { ParticipantsModule } from 'src/participants/participants.module';
-import { EventsService } from 'src/events/events.service';
-import { UsersService } from 'src/users/users.service';
+import { EventsModule } from 'src/events/events.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     ParticipantsModule,
-    EventsService,
-    UsersService
+    EventsModule,
+    UsersModule,
   ],
   controllers: [DrawsController],
   providers: [DrawsService],

171cdb53099e477fd497d6d14f2a6326eac49939
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-28 20:56:12 -0500

    Draws: Add module imports

--- src/draws/draws.module.ts
+++ src/draws/draws.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DrawsService } from './draws.service';
 import { DrawsController } from './draws.controller';
+import { ParticipantsModule } from 'src/participants/participants.module';
+import { EventsService } from 'src/events/events.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
+  imports: [
+    ParticipantsModule,
+    EventsService,
+    UsersService
+  ],
   controllers: [DrawsController],
-  providers: [DrawsService]
+  providers: [DrawsService],
+  exports: [DrawsService]
 })
 export class DrawsModule {}

19f92b64e302314c852ada0ea47f2e508953ab1c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-29 15:51:47 -0500

    Draws: Add typeorm entity

--- src/draws/draws.module.ts
+++ src/draws/draws.module.ts
@@ -4,9 +4,14 @@ import { DrawsController } from './draws.controller';
 import { ParticipantsModule } from 'src/participants/participants.module';
 import { EventsModule } from 'src/events/events.module';
 import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Draw } from './entities/draw.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([
+      Draw
+    ]),
     ParticipantsModule,
     EventsModule,
     UsersModule,

cd43bb461efbeba06c8210220a42c87b9af2ca86
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-06-28 20:52:58 -0500

    Draws: Update entity

--- src/draws/entities/draw.entity.ts
+++ src/draws/entities/draw.entity.ts
@@ -1,1 +1,21 @@
-export class Draw {}
+import { Event } from 'src/events/entities/event.entity';
+import { Participant } from 'src/participants/entities/participant.entity';
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('draws')
+export class Draw extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('datetime')
+  createAt: Date = new Date(Date.now());
+
+  @ManyToOne(() => Participant, participant => participant.drawers)
+  drawer: Participant;
+
+  @ManyToOne(() => Participant, participant => participant.drawees)
+  drawee: Participant;
+
+  @ManyToOne(() => Event, event => event.draws)
+  event: Event;
+}
--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -2,6 +2,7 @@ import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 't
 import { Participant } from 'src/participants/entities/participant.entity';
 import Link from 'src/links/entity/link.entity';
 import { Wish } from 'src/wishes/entities/wish.entity';
+import { Draw } from 'src/draws/entities/draw.entity';
 
 @Entity('events')
 export class Event extends BaseEntity {
@@ -37,4 +38,7 @@ export class Event extends BaseEntity {
 
   @OneToMany(() => Wish, wish => wish.event)
   wishes: Wish[];
+
+  @OneToMany(() => Draw, draw => draw.event)
+  draws: Draw[];
 }

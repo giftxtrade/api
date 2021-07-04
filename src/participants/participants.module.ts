c4ef26092c84136f519a7744ce0ee495ff09f618
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-07-04 14:23:21 -0400

    Participants: Add [DELETE] route

--- src/participants/participants.module.ts
+++ src/participants/participants.module.ts
@@ -3,12 +3,14 @@ import { ParticipantsService } from './participants.service';
 import { ParticipantsController } from './participants.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Participant } from './entities/participant.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Participant
-    ])
+    ]),
+    UsersModule,
   ],
   controllers: [ParticipantsController],
   providers: [ParticipantsService],

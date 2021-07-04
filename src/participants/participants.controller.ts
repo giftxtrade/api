b217a66d0b85d8f45b5d22ec9999e962758c232d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-07-04 15:38:59 -0500

    Participants: Implement [PATCH] route

--- src/participants/participants.controller.ts
+++ src/participants/participants.controller.ts
@@ -12,9 +12,25 @@ export class ParticipantsController {
     private readonly usersService: UsersService,
   ) { }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateParticipantDto: UpdateParticipantDto) {
-    return this.participantsService.update(+id, updateParticipantDto);
+  @UseGuards(JwtAuthGuard)
+  @Patch(':participantId')
+  async update(@Request() req, @Param('participantId') participantId: number, @Body() { address }: { address: string }) {
+    const user = await this.usersService.findByEmail(req.user.user.email);
+
+    const participant = await this.participantsService.findOneWithUser(participantId);
+    if (!participant) {
+      throw new HttpException({
+        message: 'Participant does not exist'
+      }, HttpStatus.NOT_FOUND);
+    }
+    if (participant.user.id !== user.id) {
+      throw new HttpException({
+        message: 'Could not update address'
+      }, HttpStatus.BAD_REQUEST);
+    }
+
+    participant.address = address;
+    return await participant.save();
   }
 
   @UseGuards(JwtAuthGuard)

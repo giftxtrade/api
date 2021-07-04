f248e92c5183d22c3c4b506e4a75ad87bb5340e0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-07-04 14:23:42 -0400

    Participants: [DELETE] route

--- src/participants/participants.controller.ts
+++ src/participants/participants.controller.ts
@@ -1,29 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, HttpStatus, HttpException } from '@nestjs/common';
 import { ParticipantsService } from './participants.service';
 import { CreateParticipantDto } from './dto/create-participant.dto';
 import { UpdateParticipantDto } from './dto/update-participant.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('participants')
 export class ParticipantsController {
-  constructor(private readonly participantsService: ParticipantsService) {}
-
-  @Get()
-  findAll() {
-    return this.participantsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.participantsService.findOne(+id);
-  }
+  constructor(
+    private readonly participantsService: ParticipantsService,
+    private readonly usersService: UsersService,
+  ) { }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateParticipantDto: UpdateParticipantDto) {
     return this.participantsService.update(+id, updateParticipantDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.participantsService.remove(+id);
+  async remove(@Request() req, @Param('id') id: number) {
+    const user = await this.usersService.findByEmail(req.user.user.email);
+    const participant = await this.participantsService.findOne(id);
+    if (!participant || participant.user.id !== user.id) {
+      throw new HttpException({
+        message: 'Could not delete participant'
+      }, HttpStatus.BAD_REQUEST);
+    }
+    return await this.participantsService.remove(id);
   }
 }

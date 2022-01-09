a05917615328d8497d5f0f2737000655cf43be98
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-08 23:50:23 -0600

    feat: update /auth/callback to return jwt token with user object

--- src/routes/auth.go
+++ src/routes/auth.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ayaanqui/go-rest-server/src/types"
 	"github.com/ayaanqui/go-rest-server/src/utils"
+	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
 	"github.com/markbates/goth/gothic"
 )
@@ -38,7 +39,28 @@ func (app *AppBase) AuthCallback(w http.ResponseWriter, r *http.Request) {
 		app.DB.Create(&new_user)
 		user_from_db = new_user
 	}
-	utils.JsonResponse(w, user_from_db)
+	
+	// Generate JWT
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id": user_from_db.ID,
+		"name": user_from_db.Name,
+		"email": user_from_db.Email,
+		"image_url": user_from_db.ImageUrl,
+	})
+	signed_token, err := token.SignedString([]byte(app.Tokens.JwtKey))
+
+	if err != nil {
+		w.WriteHeader(400)
+		utils.JsonResponse(w, types.Response{
+			Message: "Could not authenticate",
+		})
+		return
+	}
+	// Return user and token
+	utils.JsonResponse(w, types.Auth{
+		User: user_from_db,
+		Token: signed_token,
+	})
 }
 
 // AUTH REQUIRED - [GET] /auth/profile

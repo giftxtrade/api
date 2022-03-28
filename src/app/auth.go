1b86a7ba40a96b82099e48c0712f40f938a9e7c9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-27 22:35:44 -0500

    feat: return jwt and user struct on `/auth/{provider}/callback` route

--- src/app/auth.go
+++ src/app/auth.go
@@ -3,6 +3,7 @@ package app
 import (
 	"net/http"
 
+	"github.com/giftxtrade/api/src/services"
 	"github.com/giftxtrade/api/src/types"
 	"github.com/giftxtrade/api/src/utils"
 	"github.com/gorilla/mux"
@@ -28,12 +29,32 @@ func (app *AppBase) Auth(w http.ResponseWriter, r *http.Request) {
 
 // [GET] /auth/{provider}/callback
 func (app *AppBase) AuthCallback(w http.ResponseWriter, r *http.Request) {
-	user, err := gothic.CompleteUserAuth(w, r)
+	provider_user, err := gothic.CompleteUserAuth(w, r)
 	if err != nil {
 		w.WriteHeader(400)
 		utils.JsonResponse(w, types.Response{Message: "Could not complete authentication"})
 		return
 	}
+
+	check_user := types.User{
+		Email: provider_user.Email,
+		Name: provider_user.Name,
+		ImageUrl: provider_user.AvatarURL,
+	}
+	user := services.GetUserByEmailOrCreate(app.DB, provider_user.Email, &check_user)
+	token, err := utils.GenerateJWT(app.Tokens.JwtKey, &user)
+	if err != nil {
+		w.WriteHeader(400)
+		utils.JsonResponse(w, types.Response{Message: "Could not generate token"})
+		return
+	}
+	auth := types.Auth{
+		Token: token,
+		User: user,
+	}
+	utils.JsonResponse(w, auth)
+}
+
 // Auth required [GET] /auth/me
 func (app *AppBase) GetProfile(w http.ResponseWriter, r *http.Request) {
 	auth := r.Context().Value(types.AuthKey).(types.Auth)

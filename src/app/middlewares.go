258c033156954001e3821245a59666737d0ec734
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-03-27 20:39:23 -0500

    refactor: unclutter `UseJwtAuth` function

--- src/app/middlewares.go
+++ src/app/middlewares.go
@@ -9,47 +9,50 @@ import (
 	"github.com/google/uuid"
 )
 
+// Authentication middleware. Saves user data in request context within types.AuthKey key
 func UseJwtAuth(app *AppBase, next http.Handler) http.Handler {
 	const AUTH_REQ string = "Authorization required"
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		authorization := r.Header.Get("Authorization")
-		// Parse bearer token
-		raw_token, err := utils.GetBearerToken(authorization)
-		if err != nil {
-			w.WriteHeader(401)
-			utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
-			return
-		}
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			authorization := r.Header.Get("Authorization")
+			// Parse bearer token
+			raw_token, err := utils.GetBearerToken(authorization)
+			if err != nil {
+				w.WriteHeader(401)
+				utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
+				return
+			}
 
-		// Parse JWT
-		claims, err := utils.GetJwtClaims(raw_token, app.Tokens.JwtKey)
-		if err != nil {
-			w.WriteHeader(401)
-			utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
-			return
-		}
+			// Parse JWT
+			claims, err := utils.GetJwtClaims(raw_token, app.Tokens.JwtKey)
+			if err != nil {
+				w.WriteHeader(401)
+				utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
+				return
+			}
 
-		// Get user from id, username, email
-		user := types.User{}
-		app.DB.Table("users").Find(
-			&user, 
-			"id = ? AND email = ?", 
-			claims["id"],
-			claims["email"],
-		)
-		if user.ID == uuid.Nil {
-			w.WriteHeader(401)
-			utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
-			return
-		}
-		r = r.WithContext(context.WithValue(r.Context(), types.AuthKey, types.Auth{
-			Token: raw_token,
-			User: user,
-		}))
-		// Serve handler with updated request
-		next.ServeHTTP(w, r)
-	})
+			// Get user from id, username, email
+			user := types.User{}
+			app.DB.Table("users").Find(
+				&user,
+				"id = ? AND email = ?",
+				claims["id"],
+				claims["email"],
+			)
+			if user.ID == uuid.Nil {
+				w.WriteHeader(401)
+				utils.JsonResponse(w, types.Response{Message: AUTH_REQ})
+				return
+			}
+			r = r.WithContext(context.WithValue(r.Context(), types.AuthKey, types.Auth{
+				Token: raw_token,
+				User: user,
+			}))
+			// Serve handler with updated request
+			next.ServeHTTP(w, r)
+		},
+	)
 }
 
 // Admin only access middleware (uses UseJwtAuth)

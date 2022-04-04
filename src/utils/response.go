c977a2f78b2a2ff2d035c7e6b85967657d010269
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-04-03 23:44:31 -0500

    refactor: simplify response helpers + set header once

--- src/utils/response.go
+++ src/utils/response.go
@@ -7,7 +7,9 @@ import (
 	"github.com/giftxtrade/api/src/types"
 )
 
-func JsonResponse(w http.ResponseWriter, data interface{}) {
+func write_json(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		json.NewEncoder(w).Encode(
 			types.Response{
@@ -17,11 +19,20 @@ func JsonResponse(w http.ResponseWriter, data interface{}) {
 	}
 }
 
+func JsonResponse(w http.ResponseWriter, data interface{}) {
+	write_json(w, 200, data)
+}
+
 // Writes a types.Errors json response to the http.ResponseWriter,
 // with a default Http 400 status
 func FailResponse(w http.ResponseWriter, errors interface{}) {
-	w.WriteHeader(400)
-	JsonResponse(w, types.Errors{
+	write_json(w, 400, types.Errors{
+		Errors: errors,
+	})
+}
+
+func FailResponseUnauthorized(w http.ResponseWriter, errors interface{}) {
+	write_json(w, 401, types.Errors{
 		Errors: errors,
 	})
 }
@@ -29,8 +40,7 @@ func FailResponse(w http.ResponseWriter, errors interface{}) {
 // Writes a types.Data json response to the http.ResponseWriter,
 // with a default Http 200 status
 func DataResponse(w http.ResponseWriter, data interface{}) {
-	w.WriteHeader(200)
-	JsonResponse(w, types.Result{
+	write_json(w, 200, types.Result{
 		Data: data,
 	})
 }

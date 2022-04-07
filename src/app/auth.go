1617b8dc63e3a9268d8bbf43b3090c295d172ba9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-04-07 17:21:56 -0500

    feat: user services improvements

--- src/app/auth.go
+++ src/app/auth.go
@@ -40,7 +40,7 @@ func (app *AppBase) AuthCallback(w http.ResponseWriter, r *http.Request) {
 		Name: provider_user.Name,
 		ImageUrl: provider_user.AvatarURL,
 	}
-	user := services.GetUserByEmailOrCreate(app.DB, provider_user.Email, &check_user)
+	user := services.GetUserByEmailOrCreate(app.DB, &check_user)
 	token, err := utils.GenerateJWT(app.Tokens.JwtKey, &user)
 	if err != nil {
 		utils.FailResponse(w, "could not generate token")
--- src/services/users.go
+++ src/services/users.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/giftxtrade/api/src/types"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -18,23 +19,29 @@ func GetUserByEmail(db *gorm.DB, email string) types.User {
 	return user
 }
 
-func GetUserById(db *gorm.DB, id string) types.User {
+func GetUserById(db *gorm.DB, id uuid.UUID) types.User {
 	var user types.User
 	db.Table("users").Where("id = ?", id).First(&user)
 	return user
 }
 
-func GetUserByIdAndEmail(db *gorm.DB, id string, email string) types.User {
+func GetUserByIdAndEmail(db *gorm.DB, id uuid.UUID, email string) types.User {
 	var user types.User
-	db.Table("users").Where("id = ? AND email = ?", id, email).First(&user)
+	db.Table("users").Where("id = ? AND email = ?", id.String(), email).First(&user)
 	return user
 }
 
-func GetUserByEmailOrCreate(db *gorm.DB, email string, user *types.User) types.User {
-	search_user := GetUserByEmail(db, email)
+func GetUserByIdOrEmail(db *gorm.DB, id uuid.UUID, email string) types.User {
+	var user types.User
+	db.Table("users").Where("id = ? OR email = ?", id.String, email).First(user)
+	return user
+}
+
+func GetUserOrCreate(db *gorm.DB, user *types.User) types.User {
+	search_user := GetUserByIdOrEmail(db, user.ID, user.Email)
 	if search_user == (types.User{}) {
 		// create new user
-		db.Table("users").Create(user)
+		db.Table("users").Create(&user)
 		return *user
 	}
 	return search_user

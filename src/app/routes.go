f560ecfaad476d786bc5c44ee1c2e593be4f2d9e
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-04-16 13:28:22 -0500

    refactor: remove http.HanderFunc nesting

--- src/app/routes.go
+++ src/app/routes.go
@@ -10,11 +10,9 @@ import (
 
 // Create routes given a gorilla/mux router instance
 func (app *AppBase) CreateRoutes(router *mux.Router) *AppBase {
-	router.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		utils.JsonResponse(w, types.Response{
-			Message: "GiftTrade REST API ⚡",
-		})
-	})).Methods("GET")
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		utils.JsonResponse(w, types.Response{ Message: "GiftTrade REST API ⚡" })
+	}).Methods("GET")
 
 	// auth routes
 	router.Handle("/auth/profile", app.UseJwtAuth(http.HandlerFunc(app.GetProfile))).Methods("GET")

d0431004f1b7c886da1c2edc63ead82aa3d8f628
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-04-26 00:06:09 -0500

    test: add home controller test

---
+++ src/tests/controllers_test.go
@@ -0,0 +1,32 @@
+package tests
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/giftxtrade/api/src/controllers"
+	"github.com/giftxtrade/api/src/types"
+)
+
+func TestHomeController(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+	rr := httptest.NewRecorder()
+	home_controller := controllers.HomeController{}
+    handler := http.HandlerFunc(home_controller.Home)
+    handler.ServeHTTP(rr, req)
+
+    // Check the response body is what we expect.
+    expected := types.Response{Message: "GiftTrade REST API ⚡"}
+	var parsed_body types.Response
+	err = json.Unmarshal(rr.Body.Bytes(), &parsed_body)
+    if err != nil || parsed_body != expected {
+        t.Errorf("home handler did not return Response struct")
+		return
+    }
+}

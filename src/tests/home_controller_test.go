38145f8c5570c35cf9bb4d578af13e11286bc11f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-04-27 23:37:58 -0500

    refactor: home controller tests

--- src/tests/home_controller_test.go
+++ src/tests/home_controller_test.go
@@ -12,21 +12,21 @@ import (
 
 func TestHomeController(t *testing.T) {
 	req, err := http.NewRequest("GET", "/", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	rr := httptest.NewRecorder()
 	home_controller := controllers.HomeController{}
-    handler := http.HandlerFunc(home_controller.Home)
-    handler.ServeHTTP(rr, req)
+	handler := http.HandlerFunc(home_controller.Home)
+	handler.ServeHTTP(rr, req)
 
-    // Check the response body is what we expect.
-    expected := types.Response{Message: "GiftTrade REST API ⚡"}
+	// Check the response body is what we expect.
+	expected := types.Response{Message: "GiftTrade REST API ⚡"}
 	var parsed_body types.Response
 	err = json.Unmarshal(rr.Body.Bytes(), &parsed_body)
-    if err != nil || parsed_body != expected {
-        t.Errorf("home handler did not return Response struct")
+	if err != nil || parsed_body != expected {
+		t.Errorf("home handler did not return Response struct")
 		return
-    }
+	}
 }

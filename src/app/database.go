f608eec3b783b53234cdeb7bdc0e01efaec613c0
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-05-01 14:52:27 -0500

    feat: add public auto migrate function

--- src/app/database.go
+++ src/app/database.go
@@ -4,16 +4,20 @@ import (
 	"log"
 
 	"github.com/giftxtrade/api/src/types"
+	"gorm.io/gorm"
 )
 
-func (app *AppBase) CreateSchemas() {
-	err := app.DB.AutoMigrate(
+func AutoMigrate(db *gorm.DB) error {
+	return db.AutoMigrate(
 		&types.User{},
 		&types.Category{},
 		&types.Product{},
 		&types.Event{},
 	)
-	if err != nil {
+}
+
+func (app *AppBase) CreateSchemas() {
+	if err := AutoMigrate(app.DB); err != nil {
 		log.Fatal("Could not generate schema.\n")
 		panic(err)
 	}

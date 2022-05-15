24e61068a5a7bce01512594c25ceb89be9d2305e
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-05-15 00:49:03 -0500

    feat: create connection with test db

--- src/tests/connections.go
+++ src/tests/connections.go
@@ -4,43 +4,16 @@ import (
 	"fmt"
 	"testing"
 
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
-	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/giftxtrade/api/src/utils"
 )
 
-func MockDB() (*gorm.DB, sqlmock.Sqlmock, error) {
-	db_conn, mock, err := sqlmock.New()
-    if err != nil {
-		return nil, mock, fmt.Errorf("failed to open mock sql db, got error: %v", err)
-    }
-    if db_conn == nil {
-        return nil, mock, fmt.Errorf("mock db is null")
-    }
-
-    dialector := postgres.New(postgres.Config{
-        DSN:                  "sqlmock_db_0",
-        DriverName:           "postgres",
-        Conn:                 db_conn,
-        PreferSimpleProtocol: true,
-    })
-    db, err_gorm := gorm.Open(dialector, &gorm.Config{})
-    if err_gorm != nil {
-        return nil, mock, fmt.Errorf("failed to open gorm v2 db, got error: %v", err_gorm)
-    }
-
-    if db == nil {
-        return nil, mock, fmt.Errorf("gorm db is null")
-    }
-	return db, mock, nil
-}
-
-func NewMockDB(t *testing.T) (*gorm.DB, sqlmock.Sqlmock, error) {
-	db, mock, err := MockDB()
+func NewMockDB(t *testing.T) (*gorm.DB, error) {
+	db, err := utils.CreateDbConnection("localhost", "postgres", "password", "giftxtrade_test_db", "5432", false)
 	if (err != nil) {
 		fmt.Print(err)
 		t.FailNow()
 	}
-	return db, mock, nil
+	return db, nil
 }

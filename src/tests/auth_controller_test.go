cad51d0454744212302a3bc30a05c20f2da96da7
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-05-24 02:00:10 -0500

    test: auth middleware

--- src/tests/auth_controller_test.go
+++ src/tests/auth_controller_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/giftxtrade/api/src/controllers"
 	"github.com/giftxtrade/api/src/types"
+	"github.com/giftxtrade/api/src/utils"
 )
 
 func TestAuthController(t *testing.T) {
@@ -18,6 +19,41 @@ func TestAuthController(t *testing.T) {
 		Controller: *SetupMockController(user_service.DB),
 		UserServices: user_service,
 	}
+	token := "my-jwt-token"
+
+	t.Run("auth middleware", func(t *testing.T) {
+		t.Run("should throw status 401", func(t *testing.T) {
+			t.Run("no authorization header", func(t *testing.T) {
+				req, err := http.NewRequest("GET", "/auth/profile", nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+				
+				rr := httptest.NewRecorder()
+				handler := http.Handler(auth_controller.Controller.UseJwtAuth(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})))
+				handler.ServeHTTP(rr, req)
+
+				if rr.Result().StatusCode != 401 {
+					t.Fatal("status code must be a 401")
+				}
+			})
+
+			t.Run("invalid bearer token", func(t *testing.T) {
+				req, err := http.NewRequest("GET", "/auth/profile", nil)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				req.Header.Set("Authorization", "Bearer some-random-jwt")		
+				rr := httptest.NewRecorder()
+				handler := http.Handler(auth_controller.Controller.UseJwtAuth(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})))
+				handler.ServeHTTP(rr, req)
+
+				if rr.Result().StatusCode != 401 {
+					t.Fatal("status code must be a 401")
+				}
+			})
+		})
 
 	t.Run("[GET] /auth/profile", func(t *testing.T) {
 		t.Run("should return auth struct", func(t *testing.T) {
@@ -28,7 +64,7 @@ func TestAuthController(t *testing.T) {
 
 			mock_auth := types.Auth{
 				User: types.User{},
-				Token: "my-jwt-token",
+				Token: token,
 			}
 
 			req = req.WithContext(context.WithValue(

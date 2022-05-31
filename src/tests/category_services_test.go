8406ee2ad882bd74e7e376374af582a78ad8e726
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-05-30 22:13:42 -0500

    test: find category

--- src/tests/category_services_test.go
+++ src/tests/category_services_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/giftxtrade/api/src/types"
@@ -9,19 +10,23 @@ import (
 func TestCategoryServices(t *testing.T) {
 	category_services := SetupMockCategoryServices(t)
 
+	input := types.CreateCategory{
+		Name: "Fashion",
+		Url: "https://example.com",
+		Description: "Clothing and apparel",
+	}
+
 	t.Run("create category", func(t *testing.T) {
 		t.Run("should create", func(t *testing.T) {
-			input := types.CreateCategory{
-				Name: "Fashion",
-				Url: "https://example.com",
-				Description: "Clothing and apparel",
-			}
-			created, err := category_services.Create(&input)
+			t.Cleanup(func() {
+				category_services.DB.Exec("delete from categories")
+			})
+			input_created, err := category_services.Create(&input)
 			if err != nil {
 				t.Fatal(err.Error())
 			}
-			if created.Name != input.Name || created.Url != input.Url || created.Description != input.Description {
-				t.Fatal("values should be equal", created, input)
+			if input_created.Name != input.Name || input_created.Url != input.Url || input_created.Description != input.Description {
+				t.Fatal("values should be equal", input_created, input)
 			}
 		})
 
@@ -32,6 +37,23 @@ func TestCategoryServices(t *testing.T) {
 			created, err := category_services.Create(&input)
 			if err == nil || created != nil {
 				t.Fatal(err.Error())
+			}
+		})
+	})
+
+	t.Run("find category", func(t *testing.T) {
+		t.Run("should return created category", func(t *testing.T) {
+			input_created, err := category_services.Create(&input)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			found_category, err := category_services.Find(input_created.Name)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(found_category, input_created) {
+				t.Fatal(found_category, input_created)
 			}
 		})
 	})

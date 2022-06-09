7a989883caa405c2a9095b36caadf94f042f2e7f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-06-09 14:57:50 -0500

    test: product create function

--- src/tests/product_service_test.go
+++ src/tests/product_service_test.go
@@ -2,12 +2,30 @@ package tests
 
 import (
 	"testing"
+
+	"github.com/giftxtrade/api/src/types"
 )
 
 func TestProductService(t *testing.T) {
 	product_service := SetupMockProductService(t)
 
-	t.Run("", func(t *testing.T) {
-		
+	t.Run("create product", func(t *testing.T) {
+		t.Run("should not create product", func(t *testing.T) {
+			input := types.CreateProduct{}
+			product, err := product_service.Create(&input)
+			if err == nil || product != nil {
+				t.Fatalf("should not create product")
+			}
+
+			input.Title = "sample product"
+			product, err = product_service.Create(&input)
+			if err == nil || product != nil {
+				t.Fatalf(err.Error())
+			}
+		})
+
+		t.Run("should create product", func(t *testing.T) {
+			
+		})
 	})
 }

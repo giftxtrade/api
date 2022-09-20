3b084829533e90f00c42606c7293745a422448af
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-09-20 00:30:00 -0500

    feat: add create event controller

---
+++ src/controllers/event_controller.go
@@ -0,0 +1,33 @@
+package controllers
+
+import (
+	"strings"
+
+	"github.com/giftxtrade/api/src/types"
+	"github.com/giftxtrade/api/src/utils"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (ctrl Controller) CreateEvent(ctx *fiber.Ctx) error {
+	event_service := ctrl.Service.EventService
+	cur_auth := utils.ParseAuthContext(ctx.UserContext())
+
+	var input types.CreateEvent
+	if err := ctx.BodyParser(&input); err != nil {
+		return utils.FailResponse(ctx, "could not parse body")
+	}
+	validate_input := validator.New().Struct(&input)
+	if validate_input != nil {
+		errors := strings.Split(validate_input.Error(), "\n")
+		return utils.FailResponse(ctx, errors...)
+	}
+
+	var new_event types.Event
+	create_err := event_service.Create(&input, &cur_auth.User, &new_event)
+	if create_err != nil {
+		return utils.FailResponse(ctx, "could not create event")
+	}
+
+	return utils.DataResponseCreated(ctx, &new_event)
+}

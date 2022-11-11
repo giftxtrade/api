f7ee919aea21c28b982bc7fa48caf347e204901a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-11-11 11:32:21 -0600

    feat: add omit empty tag to structs

--- src/types/json.go
+++ src/types/json.go
@@ -56,20 +56,20 @@ type Auth struct {
 type CreateUser struct {
 	Name string `json:"name" validate:"required"`
 	Email string `json:"email" validate:"required,email"`
-	ImageUrl string `json:"imageUrl" validate:"omitempty,url"`
+	ImageUrl string `json:"imageUrl,omitempty" validate:"omitempty,url"`
 }
 
 type CreateCategory struct {
 	Name string `json:"name" validate:"required"`
-	Description string `json:"description"`
-	Url string `json:"url" validate:"omitempty,url"`
+	Description string `json:"description,omitempty"`
+	Url string `json:"url,omitempty" validate:"omitempty,url"`
 }
 
 type CreateProduct struct {
 	Title string `json:"title" validate:"required"`
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	ProductKey string `json:"productKey" validate:"required"`
-	ImageUrl string `json:"imageUrl" validate:"omitempty,url"`
+	ImageUrl string `json:"imageUrl,omitempty" validate:"omitempty,url"`
 	Rating float32 `json:"rating" validate:"required,min=1,max=5"`
 	Price float32 `json:"price" validate:"required,gte=1"`
 	OriginalUrl string `json:"originalUrl" validate:"required,url"`
@@ -78,27 +78,27 @@ type CreateProduct struct {
 }
 
 type ProductFilter struct {
-	Search string `json:"search" validate:"omitempty"`
+	Search string `json:"search,omitempty" validate:"omitempty"`
 	Limit int `json:"limit" validate:"required,min=1,max=200"`
 	Page int `json:"page" validate:"required,gte=1"`
-	MinPrice float32 `json:"minPrice" validate:"omitempty,gte=1,ltefield=MaxPrice"`
-	MaxPrice float32 `json:"maxPrice" validate:"omitempty,gtefield=MinPrice"`
-	Sort string `json:"sort" validate:"omitempty"`
+	MinPrice float32 `json:"minPrice,omitempty" validate:"omitempty,gte=1,ltefield=MaxPrice"`
+	MaxPrice float32 `json:"maxPrice,omitempty" validate:"omitempty,gtefield=MinPrice"`
+	Sort string `json:"sort,omitempty" validate:"omitempty"`
 }
 
 type CreateEvent struct {
 	Name string `json:"name" validate:"required"`
-	Description string `json:"description"`
+	Description string `json:"description,omitempty"`
 	Budget float32 `json:"budget" validate:"required,gte=1"`
-	InviteMessage string `json:"inviteMessage"`
+	InviteMessage string `json:"inviteMessage,omitempty"`
 	DrawAt time.Time `json:"drawAt" validate:"required"`
 	CloseAt time.Time `json:"closeAt" validate:"required"`
 }
 
 type CreateParticipant struct {
 	Email string `json:"email" validate:"required,email"`
-	Nickname string `json:"nickname" validate:"omitempty"`
-	Address string `json:"address" validate:"omitempty"`
-	Organizer bool `json:"organizer" validate:"omitempty"`
-	Participates bool `json:"participates" validate:"omitempty"`
+	Nickname string `json:"nickname,omitempty" validate:"omitempty"`
+	Address string `json:"address,omitempty" validate:"omitempty"`
+	Organizer bool `json:"organizer,omitempty" validate:"omitempty"`
+	Participates bool `json:"participates,omitempty" validate:"omitempty"`
 }

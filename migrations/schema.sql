004730b8e22678a0b7f4c6d77d727f7a82bd012f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2023-12-11 16:19:20 -0600

    Update schema.sql

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,138 +1,109 @@
 --
 -- Table structure for table categories
 --
-CREATE TABLE categories (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  description text,
-  category_url varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE category (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  category_url VARCHAR(255) NOT NULL
 );
 
 --
 -- Table structure for table draws
 --
-CREATE TABLE draws (
-  id int NOT NULL AUTO_INCREMENT,
-  created_at datetime NOT NULL,
-  drawer_id int DEFAULT NULL,
-  drawee_id int DEFAULT NULL,
-  event_id int DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY IDX_5c6f8dcbad4254f1541d043b52 (drawer_id),
-  KEY IDX_4a912e98dc9402aa438ff6e5ca (event_id),
-  KEY FK_13049315c8546dd62939d537e97 (drawee_id),
-  CONSTRAINT FK_13049315c8546dd62939d537e97 FOREIGN KEY (drawee_id) REFERENCES participants (id) ON DELETE CASCADE,
-  CONSTRAINT FK_4a912e98dc9402aa438ff6e5ca5 FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE,
-  CONSTRAINT FK_5c6f8dcbad4254f1541d043b52b FOREIGN KEY (drawer_id) REFERENCES participants (id) ON DELETE CASCADE
+CREATE TABLE draw (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  created_at DATETIME NOT NULL,
+  drawer_id BIGINT REFERENCES participant(id) NOT NULL,
+  drawee_id BIGINT REFERENCES participant(id) NOT NULL,
+  event_id BIGINT REFERENCES event(id) NOT NULL
 );
 
 --
 -- Table structure for table events
 --
-CREATE TABLE events (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  description text,
-  budget decimal(10,0) NOT NULL,
-  invitation_message text NOT NULL,
-  created_at datetime NOT NULL,
-  draw_at datetime NOT NULL,
-  close_at datetime NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE event (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  budget DECIMAL(10,0) NOT NULL,
+  invitation_message TEXT NOT NULL,
+  created_at DATETIME NOT NULL,
+  draw_at DATETIME NOT NULL,
+  close_at DATETIME NOT NULL
 );
 
 --
 -- Table structure for table links
 --
-CREATE TABLE links (
-  id int NOT NULL AUTO_INCREMENT,
-  code varchar(255) NOT NULL,
-  created_at datetime NOT NULL,
-  expiration_date datetime NOT NULL,
-  event_id int DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY IDX_52a3fa2a2c27a987ed58fd2ea4 (code),
-  KEY IDX_121cadf4bb99f7ba5dbac54997 (event_id),
-  CONSTRAINT FK_121cadf4bb99f7ba5dbac54997a FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
+CREATE TABLE link (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  code VARCHAR(255) NOT NULL,
+  created_at DATETIME NOT NULL,
+  expiration_date DATETIME NOT NULL,
+  event_id BIGINT REFERENCES event(id) NOT NULL
 );
 
 --
 -- Table structure for table participants
 --
-CREATE TABLE participants (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  address varchar(255) NOT NULL,
-  organizer tinyint NOT NULL,
-  participates tinyint NOT NULL,
-  accepted tinyint NOT NULL,
-  event_id int DEFAULT NULL,
-  user_id int DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY IDX_b77ad0832a0f8ec526c1f40a84 (email),
-  KEY IDX_a622804301e735196918e6a47e (event_id),
-  KEY IDX_5fc9cddc801b973cd9edcdda42 (user_id),
-  CONSTRAINT FK_5fc9cddc801b973cd9edcdda42a FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-  CONSTRAINT FK_a622804301e735196918e6a47e5 FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
+CREATE TABLE participant (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  organizer BOOLEAN DEFAULT false,
+  participates BOOLEAN DEFAULT true,
+  accepted BOOLEAN DEFAULT false,
+  event_id BIGINT REFERENCES event(id) NOT NULL,
+  user_id BIGINT REFERNCES user(id) NOT NULL
 );
 
 --
 -- Table structure for table products
 --
-CREATE TABLE products (
-  id int NOT NULL AUTO_INCREMENT,
-  title text NOT NULL,
-  description text NOT NULL,
-  product_key varchar(255) NOT NULL,
-  image_url text NOT NULL,
-  rating double NOT NULL,
-  price double NOT NULL,
-  currency varchar(255) NOT NULL,
-  modified datetime NOT NULL,
-  website text NOT NULL,
-  categoryId int DEFAULT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY IDX_add1c4bbc945b8fc7c3fdb4689 (product_key),
-  KEY IDX_2394b26ba3206c1c59f461d3dc (rating),
-  KEY IDX_75895eeb1903f8a17816dafe0a (price),
-  KEY IDX_ff56834e735fa78a15d0cf2192 (categoryId),
-  FULLTEXT KEY IDX_c30f00a871de74c8e8c213acc4 (title),
-  CONSTRAINT FK_ff56834e735fa78a15d0cf21926 FOREIGN KEY (categoryId) REFERENCES categories (id) ON DELETE CASCADE
+CREATE TABLE product (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT,
+  product_key VARCHAR(255) UNIQUE NOT NULL,
+  image_url TEXT NOT NULL,
+  total_reviews INT NOT NULL,
+  rating DOUBLE NOT NULL,
+  price DOUBLE NOT NULL,
+  currency VARCHAR(255) NOT NULL,
+  modified DATETIME NOT NULL,
+  website TEXT NOT NULL,
+  category_id BIGINT REFERENCES category(id) NOT NULL
 );
+
+ALTER TABLE product
+ADD COLUMN title_ts tsvector
+    GENERATED ALWAYS AS (
+    	setweight(to_tsvector('english', coalesce(title, '')), 'A') ||
+     	setweight(to_tsvector('english', coalesce(description, '')), 'B') 
+    ) STORED;
 
 --
 -- Table structure for table users
 --
-CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  image_url varchar(255) NOT NULL,
-  phone varchar(255) DEFAULT NULL,
-  password text,
-  PRIMARY KEY (id),
-  UNIQUE KEY IDX_97672ac88f789774dd47f7c8be (email)
+CREATE TABLE user (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  image_url VARCHAR(255) NOT NULL,
+  phone VARCHAR(255) DEFAULT NULL,
+  admin BOOLEAN DEFAULT false
 );
 
 --
 -- Table structure for table wishes
 --
-CREATE TABLE wishes (
-  id int NOT NULL AUTO_INCREMENT,
-  created_at datetime NOT NULL,
-  user_id int DEFAULT NULL,
-  participant_id int DEFAULT NULL,
-  product_key int DEFAULT NULL,
-  event_id int DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY IDX_4a6e5770133910acfc3c16a499 (user_id),
-  KEY IDX_12156d1b005e5d22483fd4fc08 (participant_id),
-  KEY IDX_95215d774cbe2079c9d90560dc (product_key),
-  KEY IDX_a9ed3aaf623b52ec4d11e272f3 (event_id),
-  CONSTRAINT FK_12156d1b005e5d22483fd4fc086 FOREIGN KEY (participant_id) REFERENCES participants (id) ON DELETE CASCADE,
-  CONSTRAINT FK_4a6e5770133910acfc3c16a499b FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-  CONSTRAINT FK_95215d774cbe2079c9d90560dc5 FOREIGN KEY (product_key) REFERENCES products (id) ON DELETE CASCADE,
-  CONSTRAINT FK_a9ed3aaf623b52ec4d11e272f3f FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
+CREATE TABLE wish (
+  id BIGSERIAL UNIQUE PRIMARY KEY,
+  created_at DATETIME NOT NULL,
+  user_id BIGINT REFERENCES user(id) NOT NULL,
+  participant_id BIGINT REFERENCES participant(id) NOT NULL,
+  product_id BIGINT REFERENCES product(id) NOT NULL,
+  event_id BIGINT REFERENCES event(id) NOT NULL
 );

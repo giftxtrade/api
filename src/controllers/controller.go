7efece1fd9b3ea642ad29a45f90b02a25031fa63
Ayaan Siddiqui <=>
2023-12-12 12:37:15 -0600

    refactor: move home_controller to base controller

--- src/controllers/controller.go
+++ src/controllers/controller.go
@@ -4,6 +4,8 @@ import (
 	"github.com/giftxtrade/api/src/database"
 	"github.com/giftxtrade/api/src/services"
 	"github.com/giftxtrade/api/src/types"
+	"github.com/giftxtrade/api/src/utils"
+	"github.com/gofiber/fiber/v2"
 )
 
 type Controller struct {
@@ -25,28 +27,27 @@ func New(app_ctx types.AppContext, querier *database.Queries, service services.S
 	server := app_ctx.Server
 
 	// create routes
-	server.Get("/", controller.Home)
-	
+	server.Get("/", func(c *fiber.Ctx) error {
+		return utils.JsonResponse(c, types.Response{
+			Message: "GiftTrade REST API ⚡",
+		})
+	})
 	auth := server.Group("/auth")
 	{ // auth
 		auth.Get("/profile", controller.UseJwtAuth, controller.GetProfile)
 		auth.Get("/:provider", controller.SignIn)
 		auth.Get("/:provider/callback", controller.Callback)
 	}
-
 	products := server.Group("/products")
 	{
 		products.Post("", controller.UseAdminOnly, controller.CreateProduct)
 		products.Get("", controller.UseJwtAuth, controller.FindAllProducts)
 		products.Get("/:id", controller.UseJwtAuth, controller.FindProduct)
 	}
-
-	events := server.Group("/events")
-	{
-		events.Post("", controller.UseJwtAuth, controller.CreateEvent)
-		events.Get("", controller.UseJwtAuth, controller.GetAllEvents)
-	}
-
-	server.Get("*", controller.NotFound)
+	server.Get("*", func(c *fiber.Ctx) error {
+		return utils.ResponseWithStatusCode(c, fiber.ErrNotFound.Code, types.Errors{
+			Errors: []string{"resource not found"},
+		})
+	})
 	return controller
 }

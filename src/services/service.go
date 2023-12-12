391969db6f13f474f06251a12495a75092824087
ayaanqui <ayaanqui.com@gmail.com>
2023-12-11 22:45:58 -0600

    fix: service dependencies

--- src/services/service.go
+++ src/services/service.go
@@ -3,22 +3,20 @@ package services
 import (
 	"database/sql"
 
+	"github.com/giftxtrade/api/src/database"
 	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
 type ServiceBase struct {
 	DB *sql.DB
+	Querier *database.Queries
 	Validator *validator.Validate
 }
 
 type Service struct {
 	DB *sql.DB
 	UserService UserService
-	CategoryService CategoryService
-	ProductService ProductService
-	EventService EventService
-	ParticipantService ParticipantService
 }
 
 type IService interface {
@@ -26,36 +24,17 @@ type IService interface {
 	New(db *gorm.DB) Service
 }
 
-func CreateService(db *sql.DB, validator *validator.Validate) ServiceBase {
-	return ServiceBase{
-		DB: db,
-		Validator: validator,
-	}
-}
-
-func New(db *sql.DB, validator *validator.Validate) Service {
+func New(db *sql.DB, querier *database.Queries, validator *validator.Validate) Service {
 	service := Service{
 		DB: db,
 	}
 
 	service.UserService = UserService{
-		ServiceBase: CreateService(db, validator),
-	}
-	service.CategoryService = CategoryService{
-		ServiceBase: CreateService(db, validator),
-	}
-	service.ProductService = ProductService{
-		ServiceBase: CreateService(db, validator),
-		CategoryService: service.CategoryService,
-	}
-	service.ParticipantService = ParticipantService{
-		ServiceBase: CreateService(db, validator),
-		UserService: service.UserService,
-	}
-	service.EventService = EventService{
-		ServiceBase: CreateService(db, validator),
-		UserService: service.UserService,
-		ParticipantService: service.ParticipantService,
+		ServiceBase {
+			DB: db,
+			Querier: querier,
+			Validator: validator,
+		},
 	}
 	return service
 }

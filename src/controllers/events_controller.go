4863640aec78f12f12e27fbfd6295b4c3cff0c3e
ayaanqui <ayaanqui.com@gmail.com>
2023-12-21 23:28:48 -0600

    feat: add event creation and participants logic

--- src/controllers/events_controller.go
+++ src/controllers/events_controller.go
@@ -11,27 +11,107 @@ import (
 )
 
 func (ctr *Controller) CreateEvent(c *fiber.Ctx) error {
-	var create_product types.CreateEvent
-	if c.BodyParser(&create_product) != nil {
+	auth_user := ParseAuthContext(c.Context())
+	var input types.CreateEvent
+	if c.BodyParser(&input) != nil {
 		return utils.FailResponse(c, "could not parse body data")
 	}
-	if err := ctr.Validator.Struct(create_product); err != nil {
+	if err := ctr.Validator.Struct(input); err != nil {
 		return utils.FailResponse(c, err.Error())
 	}
 
-	new_event, err := ctr.Querier.CreateEvent(c.Context(), database.CreateEventParams{
-		Name: create_product.Name,
+	tx, err := ctr.DB.BeginTx(c.Context(), nil)
+	if err != nil {
+		tx.Rollback()
+		return utils.FailResponse(c, "could not process", "error with database transaction")
+	}
+	q := ctr.Querier.WithTx(tx)
+	defer q.Close()
+
+	new_event, err := q.CreateEvent(c.Context(), database.CreateEventParams{
+		Name: input.Name,
 		Description: sql.NullString{
-			String: create_product.Description,
-			Valid: create_product.Description != "",
+			String: input.Description,
+			Valid: input.Description != "",
 		},
-		Budget: fmt.Sprintf("%f", create_product.Budget),
-		InvitationMessage: create_product.InviteMessage,
-		DrawAt: create_product.DrawAt,
-		CloseAt: create_product.CloseAt,
+		Budget: fmt.Sprintf("%f", input.Budget),
+		InvitationMessage: input.InviteMessage,
+		DrawAt: input.DrawAt,
+		CloseAt: input.CloseAt,
 	})
 	if err != nil {
+		tx.Rollback()
 		return utils.FailResponse(c, "could not create event")
 	}
-	return utils.DataResponseCreated(c, new_event)
+
+	found_creator_participant := false
+	var creator_participant types.CreateParticipant
+	participants := make([]types.Participant, len(input.Participants))
+	for i, p := range input.Participants {
+		data := database.CreateParticipantParams{
+			Name: p.Name,
+			Email: p.Email,
+			Organizer: p.Organizer,
+			Participates: p.Participates,
+			Accepted: false,
+			EventID: new_event.ID,
+			Address: sql.NullString{
+				Valid: p.Address != "",
+				String: p.Address,
+			},
+		}
+		if p.Organizer && p.Email == auth_user.User.Email {
+			found_creator_participant = true
+			creator_participant = p
+			data.UserID = sql.NullInt64{
+				Valid: true,
+				Int64: auth_user.User.ID,
+			}
+			data.Accepted = true
+		}
+
+		new_participant, err := q.CreateParticipant(c.Context(), data)
+		if err != nil {
+			tx.Rollback()
+			return utils.FailResponse(c, "could not create participant", p.Name, p.Email)
+		}
+		participants[i] = types.Participant{
+			ID: new_participant.ID,
+			Name: new_participant.Name,
+			Email: new_participant.Email,
+			Address: new_participant.Address,
+			Organizer: new_participant.Organizer,
+			Participates: new_participant.Participates,
+			Accepted: new_participant.Accepted,
+			EventID: new_event.ID,
+			UserID: new_participant.UserID.Int64,
+		}
+	}
+	if !found_creator_participant {
+		tx.Rollback()
+		return utils.FailResponse(c, fmt.Sprintf(
+			"%s (%s) must have the organizer field set to 'true'",
+			creator_participant.Name, 
+			creator_participant.Email,
+		))
+	}
+	
+	// commit all changes! create event, and all participants
+	if tx.Commit() != nil {
+		return utils.FailResponse(c, "could not commit transaction")
+	}
+	// build new event dto
+	mapped_event := types.Event{
+		ID: new_event.ID,
+		Name: new_event.Name,
+		Description: new_event.Description.String,
+		Budget: new_event.Budget,
+		InvitationMessage: new_event.Budget,
+		DrawAt: new_event.DrawAt,
+		CloseAt: new_event.CloseAt,
+		CreatedAt: new_event.CreatedAt,
+		UpdatedAt: new_event.UpdatedAt,
+		Participants: participants,
+	}
+	return utils.DataResponseCreated(c, mapped_event)
 }

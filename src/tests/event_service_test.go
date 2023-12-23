84604dc42e6902b60aca9a909698e9d836f0a763
ayaanqui <ayaanqui.com@gmail.com>
2023-12-22 20:57:35 -0600

    feat: create event creation tests

--- src/tests/event_service_test.go
+++ src/tests/event_service_test.go
@@ -32,31 +32,60 @@ func TestEventService(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	user_dto := types.User{
+		ID: user.ID,
+		Name: user.Name,
+		Email: user.Email,
+		Active: user.Active,
+	}
 
 	t.Run("create event", func(t *testing.T) {
-		event := types.CreateEvent{
-			Name: "Event 1",
-			Budget: 100.00,
-			DrawAt: time.Now(),
-			CloseAt: time.Now().Add(time.Hour * 24 * 30),
-			Participants: append(create_participants(5), types.CreateParticipant{
-				Name: user.Name,
-				Email: user.Email,
-				Organizer: true,
-				Participates: false,
-			}),
-		}
-		new_event, err := event_service.CreateEvent(context.Background(), &types.User{
-			ID: user.ID,
-			Email: user.Email,
-			Name: user.Name,
-		}, event)
-		if err != nil {
-			t.Fatal(err)
-		}
+		t.Run("correct params", func(t *testing.T) {
+			event := types.CreateEvent{
+				Name: "Event 1",
+				Budget: 100.00,
+				DrawAt: time.Now(),
+				CloseAt: time.Now().Add(time.Hour * 24 * 30),
+				Participants: append(create_participants(5), types.CreateParticipant{
+					Name: user.Name,
+					Email: user.Email,
+					Organizer: true,
+					Participates: false,
+				}),
+			}
+			new_event, err := event_service.CreateEvent(context.Background(), &user_dto, event)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-		if len(new_event.Participants) != 6 {
-			t.Fatal("not all participants were inserted")
-		}
+			if len(new_event.Participants) != 6 {
+				t.Fatal("not all participants were inserted")
+			}
+		})
+
+		t.Run("main participant not marked as organizer", func(t *testing.T) {
+			event := types.CreateEvent{
+				Name: "Event 1",
+				Budget: 100.00,
+				DrawAt: time.Now(),
+				CloseAt: time.Now().Add(time.Hour * 24 * 30),
+				Participants: append(create_participants(5), types.CreateParticipant{
+					Name: user.Name,
+					Email: user.Email,
+					Organizer: false,
+					Participates: true,
+				}),
+			}
+			_, err := event_service.CreateEvent(context.Background(), &user_dto, event)
+			if err == nil {
+				t.Fatal("event should not be created. main participant is not marked 'organizer'")
+			}
+
+			event.Participants = create_participants(20)
+			_, err = event_service.CreateEvent(context.Background(), &user_dto, event)
+			if err == nil {
+				t.Fatal("event should not be created. main participant was not provided")
+			}
+		})
 	})
 }

fdb0f754f0926371333c98f07f97edfee476eca0
ayaanqui <ayaanqui.com@gmail.com>
2023-12-31 16:33:57 -0600

    fix: explicit errors

--- src/services/participant_service.go
+++ src/services/participant_service.go
@@ -23,13 +23,11 @@ func (s *ParticipantService) BulkCreateParticipant(
 	q := s.Querier.WithTx(tx)
 
 	found_creator_participant := false
-	var creator_participant types.CreateParticipant
 	participants := make([]types.Participant, len(input))
 	for i, p := range input {
 		data := mappers.CreateParticipantToDbCreateParticipantParams(p, event)
 		if p.Organizer && p.Email == user.Email {
 			found_creator_participant = true
-			creator_participant = p
 			data.UserID = sql.NullInt64{
 				Valid: true,
 				Int64: user.ID,
@@ -48,9 +46,9 @@ func (s *ParticipantService) BulkCreateParticipant(
 	if !found_creator_participant {
 		tx.Rollback()
 		return nil, fmt.Errorf(
-			"%s (%s) must have the organizer field set to 'true'",
-			creator_participant.Name, 
-			creator_participant.Email,
+			"%s (%s) must be in the participant list and have the organizer field set to 'true'",
+			user.Name, 
+			user.Email,
 		)
 	}
 	return participants, nil

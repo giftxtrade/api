3b0fc4bdede14acd184543edd4b099934710eccb
ayaanqui <ayaanqui.com@gmail.com>
2024-02-11 21:02:43 -0600

    feat: replace `interface{}` param with generics

--- src/utils/parsers.go
+++ src/utils/parsers.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Given a JSON file, map the contents into any struct dest
-func FileMapper(filename string, dest interface{}) error {
+func FileMapper[T any](filename string, dest T) error {
 	file, err := os.ReadFile(filename)
 	if err != nil {
 		return fmt.Errorf("%s not found", filename)
